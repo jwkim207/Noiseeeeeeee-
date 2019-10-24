36c6812e87e640fa129f1f619ec0a3a515b59944
^____^ <hys2rang@gmail.com>
2019-10-24 15:01:04 +0900

    cv2 test

--- AlgorithmTest/AlgorithmTest/Program.cs
+++ AlgorithmTest/AlgorithmTest/Program.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using OpenCvSharp;
 namespace AlgorithmTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi");
-            Console.WriteLine("야생");
-            Console.WriteLine("야생마 권용백");
-            Console.WriteLine("hi");
+            Mat m = new Mat("./samples2/0.bmp");
+            Cv2.ImShow("hi",m);
+
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
         }
     }
 }
