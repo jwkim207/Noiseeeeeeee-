6b496523a6103fb8f98d98091dd6afd7bca23686
진우 김 <jwkim207@naver.com>
2019-10-24 14:41:33 +0900

    하이하이

--- CreatesampleImages.cs
+++ CreatesampleImages.cs
@@ -20,6 +20,7 @@ namespace OpenCVConsoleApp
             int width = 2256;
             int height = 2608;
             int cnt = 0;
+            //しししし
 
             for(int i = start_y; i < src.Height - height; i+=height)
             {
