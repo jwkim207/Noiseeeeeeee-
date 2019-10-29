3deaf59749750705c1ad84bc0595018b5607ab18
진우 김 <jwkim207@naver.com>
2019-10-29 10:31:09 +0900

    kkn

--- AlgorithmTest/AlgorithmTest/Program.cs
+++ AlgorithmTest/AlgorithmTest/Program.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OpenCvSharp;
+
 namespace AlgorithmTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-          
+            Console.WriteLine("Hi");
+            Console.WriteLine("야생");
+            Console.WriteLine("야생마 권용백");
+            Console.WriteLine("hi");
         }
     }
 }
